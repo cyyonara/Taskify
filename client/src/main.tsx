5b018046b2ecb3b327571f340c23e01e74cb1467
Cyrel Villanueva <villanuevacyrel30@gmail.com>
2024-08-16 16:56:32 +0800

    add basename

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import ThemeProvider from "@/providers/ThemeProvider.tsx";
-import "./index.css";
-import { BrowserRouter as Router } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster.tsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import ThemeProvider from '@/providers/ThemeProvider.tsx';
+import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from '@/components/ui/toaster.tsx';
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router>
+    <Router basename='/'>
       <QueryClientProvider client={queryClient}>
         <ThemeProvider>
           <App />
